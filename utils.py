5ce51189b8d37ff51360c42df5456769c960b26d
Anh Le <anhle@Lights-MacBook-Pro.local>
2021-08-21 14:52:44 +0700

    utils file

---
+++ utils.py
@@ -0,0 +1,32 @@
+INPUTS = """
+5
+1 2
+3 4
+5 6
+7 8
+9 0"""
+
+def get_input(inputs):
+    # get_input(INPUTS)(input)
+    def yield_input(docstr=INPUTS):
+        def generate_input(docstr):
+            for inp in docstr.split("\n"):
+                if inp != '':
+                    yield inp
+        inputs = generate_input(docstr)
+        return inputs
+
+    ins = yield_input(inputs)
+
+    def inner(f):
+        def wrapper():
+            return next(ins)
+        return wrapper
+    return inner
+
+if __name__ == '__main__':
+    print(input())
+    print(input())
+    print(input())
+    print(input())
+    print(input())
