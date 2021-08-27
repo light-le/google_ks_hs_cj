3ac59b41f214f09bdb61cd9ae299f9c61f8ab455
Anh Le <quanganh@infinitelambda.com>
2021-08-27 15:39:19 +0700

    finish solve3

--- shuffled_anagram.py
+++ shuffled_anagram.py
@@ -1,6 +1,7 @@
 from utils import get_input
 from random import shuffle
 from collections import Counter
+from math import ceil
 
 INPUTS = """
 8
@@ -62,9 +63,10 @@ class Letter:
 
         self.count = count
         self.accumulate = None
+        self.increment = 0
 
     def __repr__(self) -> str:
-        return f'{self.label}: acc {self.accumulate}'
+        return f'{self.label}: {self.sorted} {self.swapped} {self.origin}'
 
     def __eq__(self, o: object) -> bool:
         return self.label == o.label
@@ -74,17 +76,36 @@ class Letter:
 
 
 def solve3(s):
+    """
+    This solution follows the post-event analysis
+    """
     seq = [Letter(c, i) for i, c in enumerate(s)]
     count = Counter(seq)
     most_common_count = count.most_common()
     if most_common_count[0][1] > len(s)/2:
         return "IMPOSSIBLE"
+
     sortu = sorted([Letter(letter.label, count=cnt) for letter, cnt in most_common_count], key=lambda l: l.label)
+    sortd = {letter.label: letter for letter in sortu}
     acc = 0
     for letter in sortu:
         letter.accumulate = acc
         acc+=letter.count
-    return sortu
+    sortseq = [None for _ in seq]
+    for letter in seq:
+        letter.sorted = sortd[letter.label].accumulate + sortd[letter.label].increment
+        sortd[letter.label].increment+=1
+        if letter.sorted < int(len(s)/2):
+            letter.swapped = letter.sorted + ceil(len(s)/2)
+        else:
+            letter.swapped = letter.sorted - int(len(s)/2)
+        sortseq[letter.sorted] = letter
+    ans = ['' for _ in seq]
+    for letter in sortseq:
+        final = sortseq[letter.swapped].origin
+        ans[final] = letter.label
+    assert is_shuffled_anagram(s, ans), "WRONG ANSWER"
+    return ''.join(ans)
 
 
 for case in range(1, int(input())+1):
